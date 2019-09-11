6370bd2611158169652f3ca94f2bd2313c55f99d
anton.kostiuchkov <anton.kostiuchkov@collibra.com>
2019-09-11 04:04:39 +0200

    finished 14

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,23 @@
-// let's go!
+require("dotenv").config({
+  path: "variables.env"
+});
+
+const createServer = require("./createServer");
+const db = require("./db");
+
+const server = createServer();
+
+// Use express middleware to handle cookies (JWT)
+// User express middleware to populate current user
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    }
+  },
+  (deets) => {
+    console.log(`server is now running on port http://localhost:${deets.port}`);
+  }
+);
